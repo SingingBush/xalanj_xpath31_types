79c6f7d4404ee5cc3be00ad1cecc7d7633092a23
singingbush <singingbush@hotmail.com>
2025-12-05 09:12:16 +0000

    Setup maven build

---
+++ test/xml/xpath31/processor/types/XSDateTimeTest.java
@@ -0,0 +1,63 @@
+package xml.xpath31.processor.types;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.xml.transform.TransformerException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XSDateTimeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "2025-02-28T10:00:00",
+        "2025-02-28T10:00:00.0000", // the parser does support milliseconds
+        "2025-02-28T10:00:00.0000000", // the parser doesn't really support milliseconds, just takes first 4 digits
+        "2025-02-28T10:00:00Z",
+        "2025-02-28T08:00:00+02:00",
+        "2025-02-28T12:00:00-02:00",
+    })
+    void parseDateTime(final String strVal) throws TransformerException {
+        final XSDateTime dateTime = XSDateTime.parseDateTime(strVal);
+        assertEquals(2025, dateTime.year());
+        assertEquals(2, dateTime.month());
+        assertEquals(28, dateTime.day());
+
+        assertEquals(0, dateTime.minute());
+
+        // all the tested values are actually the same time
+        dateTime.equals(XSDateTime.parseDateTime("2025-02-28T10:00:00Z"));
+
+        if (!strVal.endsWith("0000")) {
+            // string value doesn't support milliseconds or microseconds. Is this a bug?
+            assertEquals(strVal, dateTime.stringValue());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "2025-02-28T10:00", // no seconds
+        "2025-02-28T08:00:00+02" // offset needs hours and minutes
+    })
+    void parseDateTimeUnsupportedFormat(final String strVal) {
+        assertThrows(TransformerException.class, () -> XSDateTime.parseDateTime(strVal));
+    }
+
+    @Test
+    void lt() throws TransformerException {
+        final XSDateTime jan = XSDateTime.parseDateTime("2025-01-28T00:00:00Z");
+        final XSDateTime feb = XSDateTime.parseDateTime("2025-02-28T00:00:00Z");
+
+        assertTrue(jan.lt(feb));
+    }
+
+    @Test
+    void gt() throws TransformerException {
+        final XSDateTime jan = XSDateTime.parseDateTime("2025-01-28T00:00:00Z");
+        final XSDateTime feb = XSDateTime.parseDateTime("2025-02-28T00:00:00Z");
+        assertTrue(feb.gt(jan));
+    }
+
+}
